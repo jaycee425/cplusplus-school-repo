08885bea12b5b0fc5202e8da98b6ee0b308a7e92
unknown <julianbannato@gmail.com>
2019-10-14 20:29:18 +0200

    divisione

--- divisioneconsottrazionesuccesive.cpp
+++ divisioneconsottrazionesuccesive.cpp
@@ -1,1 +1,30 @@
-
+#include<stdio.h>
+int main(){
+	//dichiarazione variabili
+	int dividendo;
+	int divisore;
+	int quoto, resto;
+	int differenza;
+	int sottrazione;
+	
+	sottrazione=0; //inizializzazione a 0
+	
+	// ciclo
+	
+	do{
+		printf("inserisci il dividendo\n");
+		scanf("%d",&dividendo);
+		printf("inserisci il divisore\n");
+		scanf("%d",&divisore);
+		//calcoli
+		differenza=dividendo-divisore;
+		printf("sei arrivato a: %d - %d= %d\n",dividendo,divisore,differenza);
+		//ninserimenti
+		sottrazione++;
+			
+	}while(differenza>=divisore);
+	printf("il quoziente e': %d\n",sottrazione);
+	printf("il resto e': %d",differenza);
+	
+	
+}
