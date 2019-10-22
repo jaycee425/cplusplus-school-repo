e91c4d95b3fa7c619fa16f55742b515daee85484
Gian Carlo <jaycee@presenceKits-Mac-mini.local>
2019-10-22 23:48:59 +0200

    correzioni

--- giornopasstraduedate.cpp
+++ giornopasstraduedate.cpp
@@ -1,5 +1,7 @@
 /*
-		oggetto:dato giorno mese dell'anno in corso dire quanti giorni sono passati
+		oggetto:
+		Inserita la prima data il programma ritorna i giorni passati dall'inizio dell'anno. 
+		Inserita la seconda data il programma ritorna i giorni passati dall'inizio dell'anno e la differenza in giorni tra la prima e la seconda data.
 		dall'inizio dell'anno
 		nome:Julian Bandril
 */
@@ -8,109 +10,110 @@
 int main(){
 	//dichiarazione variabile
 	int mese;
-	int Giorno;
+	int giorno;
 	int giorniPassati=0;
 	int giorniPassati2=0;
-	int Giorno2;
+	int giorno2;
 	int intervalloGiorni;
 	int mese2;
 	
+	printf("Inserita la prima data il programma ritorna i giorni passati dall'inizio dell'anno.\n");
+	printf("Inserita la seconda data il programma ritorna i giorni passati dall'inizio dell'anno e la differenza in giorni tra la prima e la seconda data.\n\n");
+	
 	do{
-		printf("non fare lo scemo\n");
+		printf("Inserimento prima data: \n");
 		printf("inserisci il giorno:");
-		scanf("%d",&Giorno);
+		scanf("%d",&giorno);
 		printf("inserisci il mese:");
 		scanf("%d",&mese);
-	}while(Giorno>=31 or mese>=12);
+	}while(giorno>=31 || mese>=12);
 	
-	
-	if(mese==1){
-		giorniPassati=Giorno;	
+	do{
+		printf("Inserimento seconda data: \n");
+		printf("inserisci il giorno:");
+		scanf("%d",&giorno2);
+		printf("inserisci il mese:");
+		scanf("%d",&mese2);
+	}while(giorno2>=31 || mese2>=12);
+
+	if(mese>=1){
+		giorniPassati=giorno;	
 	}
-	if(mese==2){
-		giorniPassati=Giorno+31;
+	if(mese>=2){
+		giorniPassati=giorniPassati+31;
 	}
-	if(mese==3){
-		giorniPassati=Giorno+31+28;
+	if(mese>=3){
+		giorniPassati=giorniPassati+28;
 	}
-	if(mese==4){
-		giorniPassati=Giorno+31+28+31;
+	if(mese>=4){
+		giorniPassati=giorniPassati+31;
 	}
 	if(mese==5){
-		giorniPassati=Giorno+31+28+31+30;
+		giorniPassati=giorniPassati+30;
 	}
 	if(mese==6){
-		giorniPassati=Giorno+31+28+31+30+31;
+		giorniPassati=giorno+31+28+31+30+31;
 	}
 	if(mese==7){
-		giorniPassati=Giorno+31+28+31+30+31+30;
+		giorniPassati=giorno+31+28+31+30+31+30;
 	}
 	if(mese==8){
-		giorniPassati=Giorno+31+28+31+30+31+30+31;
+		giorniPassati=giorno+31+28+31+30+31+30+31;
 	}
 	if(mese==9){
-		giorniPassati=Giorno+31+28+31+30+31+30+31+31;
+		giorniPassati=giorno+31+28+31+30+31+30+31+31;
 	}
 	if(mese==10){
-		giorniPassati=Giorno+31+28+31+30+31+30+31+31+30;
+		giorniPassati=giorno+31+28+31+30+31+30+31+31+30;
 	}
 	if(mese==11){
-		giorniPassati=Giorno+31+28+31+30+31+30+31+31+30+31;
+		giorniPassati=giorno+31+28+31+30+31+30+31+31+30+31;
 	}
 	if(mese==12){
-		giorniPassati=Giorno+31+28+31+30+31+30+31+31+30+31+30;
+		giorniPassati=giorno+31+28+31+30+31+30+31+31+30+31+30;
 	}
 	
 	printf("sono passati %d giorni dall'inizio dell'anno\n",giorniPassati);
 	
-	do{
-		printf("non fare lo scemo\n");
-		printf("inserisci il giorno:");
-		scanf("%d",&Giorno2);
-		printf("inserisci il mese:");
-		scanf("%d",&mese2);
-	}while(Giorno2>=31 or mese2>=12);
-	
-	
 	if(mese2==1){
-		giorniPassati2=Giorno2;	
+		giorniPassati2=giorno2;	
 	}
 	if(mese2==2){
-		giorniPassati2=Giorno2+31;
+		giorniPassati2=giorno2+31;
 	}
 	if(mese2==3){
-		giorniPassati2=Giorno2+31+28;
+		giorniPassati2=giorno2+31+28;
 	}
 	if(mese2==4){
-		giorniPassati2=Giorno2+31+28+31;
+		giorniPassati2=giorno2+31+28+31;
 	}
 	if(mese2==5){
-		giorniPassati2=Giorno2+31+28+31+30;
+		giorniPassati2=giorno2+31+28+31+30;
 	}
 	if(mese2==6){
-		giorniPassati2=Giorno2+31+28+31+30+31;
+		giorniPassati2=giorno2+31+28+31+30+31;
 	}
 	if(mese==7){
-		giorniPassati2=Giorno2+31+28+31+30+31+30;
+		giorniPassati2=giorno2+31+28+31+30+31+30;
 	}
 	if(mese2==8){
-		giorniPassati2=Giorno2+31+28+31+30+31+30+31;
+		giorniPassati2=giorno2+31+28+31+30+31+30+31;
 	}
 	if(mese2==9){
-		giorniPassati2=Giorno2+31+28+31+30+31+30+31+31;
+		giorniPassati2=giorno2+31+28+31+30+31+30+31+31;
 	}
 	if(mese2==10){
-		giorniPassati2=Giorno2+31+28+31+30+31+30+31+31+30;
+		giorniPassati2=giorno2+31+28+31+30+31+30+31+31+30;
 	}
 	if(mese2==11){
-		giorniPassati2=Giorno2+31+28+31+30+31+30+31+31+30+31;
+		giorniPassati2=giorno2+31+28+31+30+31+30+31+31+30+31;
 	}
 	if(mese2==12){
-		giorniPassati2=Giorno2+31+28+31+30+31+30+31+31+30+31+30;
+		giorniPassati2=giorno2+(31*6)+(30*4)+28; 
 	}
 	
 	printf("sono passati %d giorni dall'inizio dell'anno\n",giorniPassati2);
-	//permette di sapere se la prima data è prima o dopo alla seconda data
+	//permette di sapere se la prima data ï¿½ prima o dopo alla seconda data
 	if(giorniPassati>giorniPassati2){
 		intervalloGiorni=giorniPassati-giorniPassati2;
 	}else{
