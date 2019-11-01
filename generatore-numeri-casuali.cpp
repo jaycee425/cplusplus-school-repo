7c6147dbc3ecea3c498a8d7248170a3efc3e8d61
Gian Carlo <jaycee@presenceKits-Mac-mini.local>
2019-11-01 10:54:29 +0100

    correzioni

--- generatore-numeri-casuali.cpp
+++ generatore-numeri-casuali.cpp
@@ -7,32 +7,22 @@ int main(){
 	int i;
 	int somma=0;
 	float media;
-	int Vmax; //valore massimo
-	int Vmin; //valore basso
+	int Vmax = 0; //valore massimo
+	int Vmin = 0; //valore minimo
 		
 	for(i=0;i<n;i++){
 		num[i]=rand()%21;
 	    printf("%d ",num[i]);
 		somma=somma+num[i];
-		
-	}
-	
-	Vmax=num[0];
-	Vmin=num[0];
-	
-	for(i=0;i<n;i++){
 		if(Vmax<num[i]){
 			Vmax=num[i];
 		}	
-			if(Vmin>num[i]){
-				Vmin=num[i];
-			}
+		if(Vmin>num[i]){
+			Vmin=num[i];
+		}
 	}
 	media=somma/n;
-	printf("\nmedia: %d\n",media);
+	printf("\nmedia: %f\n", media);
 	printf("valore massimo: %d\n",Vmax);
-	printf("valore minimo: %d\n",Vmin);
-	
-	
-		
+	printf("valore minimo: %d\n",Vmin);	
 }
